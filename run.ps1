19d4b6d06f58d22c7cc31c3a8c1fedb2d67de107
fschwiet <fschwiet@gmail.com>
2012-11-03 21:38:12 -0700

    fix up test command

---
+++ run.ps1
@@ -0,0 +1,9 @@
+msbuild
+if (test-path .\temp) { rmdir -force -recurse .\temp }
+$null = mkdir .\temp
+$gifenstein = ".\Gifenstein\bin\Debug\Gifenstein.exe"
+& $gifenstein
+& $gifenstein no-op .\media\look_how_unrustled.gif .\temp\noop.gif
+& $gifenstein info .\temp\noop.gif
+& $gifenstein append-gifs -n .\media\look_how_unrustled.gif -n .\media\look_how_unrustled.gif -o .\temp\append.gif
+& $gifenstein alright-gentlemen -o c:\temp\alright-gentleman.gif -m .\media\look_how_unrustled.gif -m '.\media\haters gonna hate.gif' -w '.\media\not the bees.gif' -t "we're going to master the gif technology"
