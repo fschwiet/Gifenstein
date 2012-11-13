ce74914c12f93c7720eaf10a16f2c896b1fd0d3f
fschwiet <fschwiet@gmail.com>
2012-11-12 21:41:51 -0800

    starting ohai command

---
+++ Gifenstein/Cheezburger/OhaiCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using ManyConsole;
+
+namespace Gifenstein.Cheezburger
+{
+    public class OhaiCommand : ConsoleCommand
+    {
+        public OhaiCommand()
+        {
+            this.IsCommand("chz-ohai", "Checks API access to the cheezburger API");
+        }
+        public override int Run(string[] remainingArguments)
+        {
+            using(var client = new WebClient())
+            {
+                var result = client.DownloadString("https://api.cheezburger.com/v1/ohai");
+                Console.WriteLine(result);
+            }
+
+            return 0;
+        }
+    }
+}
--- Gifenstein/Program.cs
+++ Gifenstein/Program.cs
@@ -7,7 +7,17 @@ namespace Gifenstein
         static int Main(string[] args)
         {
             var commands = ManyConsole.ConsoleCommandDispatcher.FindCommandsInSameAssemblyAs(typeof (Program));
-            return ManyConsole.ConsoleCommandDispatcher.DispatchCommand(commands, args, Console.Out);
+            try
+            {
+                return ManyConsole.ConsoleCommandDispatcher.DispatchCommand(commands, args, Console.Out);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception thrown:");
+                Console.WriteLine(e.ToString());
+
+                return -1;
+            }
         }
     }
 }
