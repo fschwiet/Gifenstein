9475a270c177d787a486fb40e100e7230e0641db
fschwiet <fschwiet@gmail.com>
2012-11-23 16:35:42 -0800

    trying out asp.net 4.0 bundling

---
+++ Frontpage/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+
+namespace Frontpage.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            var jsBundle = new ScriptBundle("~/bundles/js")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include(
+                    "~/Scripts/jquery.ui.core.js",
+                    "~/Scripts/jquery.ui.widget.js",
+                    "~/Scripts/jquery.ui.mouse.js", 
+                    "~/Scripts/jquery.ui.sortable.js");
+
+            bundles.Add(jsBundle);
+        }
+    }
+}
