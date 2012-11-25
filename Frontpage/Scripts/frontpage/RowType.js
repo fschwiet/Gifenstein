7e89ba81ffc13afae3107c23854821049d494d74
fschwiet <fschwiet@gmail.com>
2012-11-25 14:15:46 -0800

    have an edit button

---
+++ Frontpage/Scripts/frontpage/RowType.js
@@ -0,0 +1,32 @@
+﻿
+function ImageRow() {
+}
+
+ImageRow.prototype.Render = function () {
+    var result = $("<li><img></li>");
+    result.find("img").attr("src", this.imageUrl);
+    return result;
+};
+
+ImageRow.prototype.StartEdit = function()
+{
+    alert("clicked edit");
+}
+
+function AllRightAnnouncement() {
+    this.imageUrl = "/Content/AllRightGentlemen_dialog.png";
+}
+
+$.extend(AllRightAnnouncement.prototype, ImageRow.prototype);
+
+function AllRightMinor() {
+    this.imageUrl = "/Content/AllRightGentlemen_unimpressed.png";
+}
+
+$.extend(AllRightMinor.prototype, ImageRow.prototype);
+
+function AllRightMajor() {
+    this.imageUrl = "/Content/AllRightGentlemen_impressed.png";
+}
+
+$.extend(AllRightMajor.prototype, ImageRow.prototype);
