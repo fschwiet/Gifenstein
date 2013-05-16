57c7d65b337069af4f1ba293f3618f291eaa20af
fschwiet <fschwiet@gmail.com>
2013-05-15 20:15:28 -0700

    can load via http

--- Gifenstein/ImageResizerUtil.cs
+++ Gifenstein/ImageResizerUtil.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using ImageResizer;
 using ImageResizer.Plugins;
@@ -23,6 +24,12 @@ namespace Gifenstein
 
             var imageBuilder = new ImageBuilder(builderConfig.Plugins.ImageBuilderExtensions, builderConfig.Plugins,
                 builderConfig.Pipeline, builderConfig.Pipeline);
+
+            Console.WriteLine("processing " + source);
+            if (source is string && source.ToString().StartsWith("http"))
+            {
+                source = new MemoryStream(new WebClient().DownloadData(source.ToString()));
+            }
 
             imageBuilder.Build(source, target, resizeSettings, false);
         }
